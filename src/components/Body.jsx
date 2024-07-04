5d087a12829804e18a96c6c89d026b39c1037881
Justin Zou <justclaner@gmail.com>
2024-07-04 19:03:32 -0400

    update about this site

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -105,7 +105,8 @@ function Body(props) {
                 <>
                     <p className="body-content" style={{margin: "0 auto"}}>This site was made using HTML, CSS, JavaScript, and React Library. I made this portfolio project as 
                         an exercise to learn React for the first time. At first, I felt like React was overcomplicating a lot of concepts, but I soon realized its effectiveness
-                        in dynamic programming. It's as if I've discovered a for loop for writing complex containers filled with many elements and various styles.
+                        in dynamic programming. It's as if I've discovered a for loop for writing complex containers filled with many elements and various styles. This is just one
+                        step in my journey to learn more concepts and have better practice in both front-end in back-end development.
                     </p>
         
                 
